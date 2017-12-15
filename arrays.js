4c66e22b22a4afeeb1755d0603e8406a92c5456d
Emily Garrison <emily.e.garrison@gmail.com>
2017-12-15 16:30:27 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,6 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
-var array = [1];
+var array = [1, 2, 3];
 
 function addElementToBeginningOfArray (array, element) {
   return ["foo", ...array];
@@ -24,16 +24,12 @@ function accessElementInArray(array, index) {
   return array[index];
 }
 
-var array2 = [1, 2, 3];
-
 function destructivelyRemoveElementFromBeginningOfArray(array) {
-  array = array2;
   array.shift();
   return array;
 }
 
 function removeElementFromBeginningOfArray(array) {
-  array = array2;
   array2.slice(0);
   return array2;
 }
